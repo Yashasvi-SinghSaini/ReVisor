dbd76984e9aa7ac2f289f8d996abc943a548f1b5
Yashasvi-SinghSaini <yashasvisinghsaini2007+github@gmail.com>
2024-02-03 09:46:41 +0530

    add prv_day_checkbox

--- ReVisor/ReVisor.py
+++ ReVisor/ReVisor.py
@@ -28,7 +28,9 @@ CTkLabel(master=frame_3, text="WHAT DID YOU STUDY TODAY?",text_color='#ccddff', 
 Chapter = CTkEntry(master=frame_3, placeholder_text="Chapter?",text_color='#ffb3ff', width=400, font=("Cascadia Mono", 15))
 Topic = CTkEntry(master=frame_3, placeholder_text="Topic?",text_color='#ffb3ff', width=400, font=("Cascadia Mono", 15))
 button = CTkButton(master=frame_3, text="Add", command=button_func, font=("Cascadia Mono", 15))
+prv_day_check = CTkCheckBox(master=frame_3, text="Add for previous day?", font=("Cascadia Mono", 15), corner_radius=20)
 
+prv_day_check.pack()
 Chapter.pack(expand=True, pady=15, padx=20)
 Topic.pack(expand=True, pady=15, padx=20)
 button.pack(expand=True, fill="both", pady=(30, 15), padx=30)
